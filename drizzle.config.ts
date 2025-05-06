b0d0e3b826a1e9c45f7eff8d987cf099dc04b712
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2025-05-06 20:06:17 +0530

    Resolve todo

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     schema: "./src/db/schema.ts",
-    out: "./drizzle", // @TODO: What is this for?
+    out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
