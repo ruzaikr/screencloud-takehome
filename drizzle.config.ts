632b7abd69dfa5711cbfe8e37e02137a252cce83
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2025-05-22 14:47:40 +0530

    Update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-        url: `${process.env.DATABASE_URL!}?sslmode=no-verify`
+        host: process.env.DATABASE_HOST!,
+        port: parseInt(process.env.DATABASE_PORT!),
+        user: process.env.DATABASE_USER!,
+        password: process.env.DATABASE_PASSWORD!,
+        database: process.env.DATABASE_NAME!,
+        ssl: { ca: process.env.DATABASE_CA! },
     }
 })
