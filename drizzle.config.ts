96776715f31f167e497537de03175e429fb8071b
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2025-05-22 21:02:08 +0530

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
+const url =
+    process.env.NODE_ENV === "test"
+        ? `${process.env.DATABASE_URL!}?sslmode=disable`
+        : `${process.env.DATABASE_URL!}?sslmode=no-verify`;
+
 export default defineConfig({
     schema: "./src/db/schema.ts",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-        url: `${process.env.DATABASE_URL!}?sslmode=no-verify`
+        url: url
     }
 })
--- src/db/client.ts
+++ src/db/client.ts
@@ -11,10 +11,6 @@ const connectionString =
         ? `${config.DATABASE_URL!}?sslmode=disable`
         : `${config.DATABASE_URL!}?sslmode=no-verify`;
 
-console.error(`node_env is ${config.NODE_ENV}`);
-console.error(`database url is ${config.DATABASE_URL}`);
-console.error(`connection string is ${connectionString}`);
-
 const pool = new Pool({
     connectionString: connectionString,
 });
