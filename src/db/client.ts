ed672b83ffdd4904d72b089e92c861e1b85ddfb5
Ruzaik Rafeek <ruzaik.rafeek@gmail.com>
2025-05-22 20:53:30 +0530

    add debug logs

--- src/db/client.ts
+++ src/db/client.ts
@@ -11,6 +11,10 @@ const connectionString =
         ? `${config.DATABASE_URL!}?sslmode=disable`
         : `${config.DATABASE_URL!}?sslmode=no-verify`;
 
+console.log(`node_env is ${config.NODE_ENV}`);
+console.log(`database url is ${config.DATABASE_URL}`);
+console.log(`connection string is ${connectionString}`);
+
 const pool = new Pool({
     connectionString: connectionString,
 });
